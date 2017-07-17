cec7ac548fe3393afa27afa8cca42ddea0d7228a
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-07-17 13:53:50 -0500

    Create Add.h

---
+++ Concept/HeaderGuard/Add.h
@@ -0,0 +1,10 @@
+// This is start of the header guard.  ADD_H can be any unique name.  By convention, we use the name of the header file.
+#ifndef ADD_H
+#define ADD_H
+
+// This is the content of the .h file, which is where the declarations go
+int add(int x, int y);
+
+
+// This is the end of the header guard
+#endif
