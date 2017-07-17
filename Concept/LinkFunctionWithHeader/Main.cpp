4e445fdf9984b03b5cc6209344e55a1a2b736a62
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-07-17 13:56:28 -0500

    Create Main.cpp

---
+++ Concept/LinkFunctionWithHeader/Main.cpp
@@ -0,0 +1,15 @@
+#include "Chpt1io.h"
+// OR BELOW
+//int readNumber();
+//
+//void writeAnswer(int x);
+
+int main()
+{
+	int c(7);
+	int a{ 2 };
+	int x = readNumber();
+	int y = readNumber();
+	writeAnswer(x + y);
+	return 0;
+}
