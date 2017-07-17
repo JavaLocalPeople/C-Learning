e7ebc8e17e0d9db815cb37f9d20304c6c740a5f1
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-07-17 14:00:39 -0500

    Update Main.cpp

--- Concept/Namespace/Main.cpp
+++ Concept/Namespace/Main.cpp
@@ -1,1 +1,11 @@
 //
+#include "Mathadd.h" // import BasicMath::add()
+#include "Mathsubtract.h" // import BasicMath::subtract()
+#include <iostream>
+int main(void)
+{
+	std::cout << BasicMath::add(4, 3) << '\n';
+	std::cout << BasicMath::subtract(4, 3) << '\n';
+
+	return 0;
+}
