2361df3604a5d90cd10adc46b6c9f539cb2f31b0
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-07-20 10:31:47 -0500

    Create CalculateMain.cpp

---
+++ Concept/Switch/CalculateMain.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+
+int calculate(int x, int y, char op)
+{
+	switch (op)
+	{
+	case '+':
+		return x + y;
+	case '-':
+		return x - y;
+	case '*':
+		return x * y;
+	case '/':
+		return x / y;
+	case '%':
+		return x % y;
+	default:
+		std::cout << "calculate(): Unhandled case\n";
+		break;
+	}
+}
+
+int main()
+{
+	std::cout << "Enter an integer: ";
+	int x;
+	std::cin >> x;
+
+	std::cout << "Enter an integer: ";
+	int y;
+	std::cin >> y;
+
+	std::cout << "Enter a mathmatical operator (+, -, *, /, or %): ";
+	char op;
+	std::cin >> op;
+
+	std::cout << x << " " << op << " " << y << " is " << calculate(x, y, op) << "\n";
+
+	return 0;
+}
