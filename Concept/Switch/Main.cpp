c9d14e83ffbdb2fb69d1c36d34dc7e518a219a83
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-07-20 10:11:44 -0500

    Update Main.cpp

--- Concept/Switch/Main.cpp
+++ Concept/Switch/Main.cpp
@@ -32,6 +32,26 @@ void printColor(Colors color)
 	}
 }
 
+bool isDigit(char c)
+{
+    switch (c)
+    {
+        case '0': // if c is 0
+        case '1': // or if c is 1
+        case '2': // or if c is 2
+        case '3': // or if c is 3
+        case '4': // or if c is 4
+        case '5': // or if c is 5
+        case '6': // or if c is 6
+        case '7': // or if c is 7
+        case '8': // or if c is 8
+        case '9': // or if c is 9
+            return true; // then return true
+        default:
+            return false;
+    }
+}
+
 int main()
 {
 	printColor(COLOR_GREEN);
