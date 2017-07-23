f3bad3819fafae9175b456d5b8d1325e7fa78832
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-07-22 21:07:04 -0500

    Create Main.cpp

---
+++ Concept/ArrayWithEnum/Main.cpp
@@ -0,0 +1,17 @@
+enum StudentNames
+{
+    KENNY, // 0
+    KYLE, // 1
+    STAN, // 2
+    BUTTERS, // 3
+    CARTMAN, // 4
+    MAX_STUDENTS // 5
+};
+ 
+int main()
+{
+    int testScores[MAX_STUDENTS]; // allocate 5 integers
+    testScores[STAN] = 76;
+ 
+    return 0;
+}
