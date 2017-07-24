fb1c96be12a298b81f98bedee00d408b75c5b33d
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-07-24 11:32:23 -0500

    Create Main.cpp

---
+++ Concept/Pointer/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	int value = 5;
+	int *ptr = &value; // initialize ptr with address of variable value.
+	std::cout << &value << '\n'; // print the address of variable value
+	std::cout << ptr << '\n'; // print the address that ptr is holding
+}
