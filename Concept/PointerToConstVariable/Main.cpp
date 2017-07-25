7120845fa80be27572e3367277dca302649b13b5
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-07-25 17:04:40 -0500

    Update Main.cpp

--- Concept/PointerToConstVariable/Main.cpp
+++ Concept/PointerToConstVariable/Main.cpp
@@ -1,3 +1,8 @@
 const int value = 5;
 const int *ptr = &value; // this is okay, ptr is pointing to a "const int"
 *ptr = 6; // not allowed, we can't change a const value
+
+int value = 5;
+const int *ptr1 = &value; // ptr1 points to a "const int", so this is a pointer to a const value.
+int *const ptr2 = &value; // ptr2 points to an "int", so this is a const pointer to a non-const value.
+const int *const ptr3 = &value; // ptr3 points to a "const int", so this is a const pointer to a const value.
