def999a66298ffa0d77b879dd7c5297a66d98aa6
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-07-26 15:21:57 -0500

    Create Main.cpp

---
+++ Concept/PassByReference/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+void foo(int &value)
+{
+	value = 6;
+}
+
+int main()
+{
+	int value = 5;
+
+	std::cout << "value = " << value << '\n';
+	foo(value);
+	std::cout << "value = " << value << '\n';
+	return 0;
+
+}
