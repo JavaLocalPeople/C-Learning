cd40b565d7f99c362931c5db73697f74edd990dd
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-07-27 10:09:27 -0500

    Create Main.cpp

---
+++ Concept/PassByAddress/Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void foo(int *ptr)
+{
+	*ptr = 6;
+}
+
+void printArray(const int *array, int length)
+{
+	// if user passed in a null pointer for array, bail out early!
+	if (!array)
+	{
+		return;
+	}
+	for (int index = 0; index < length; ++index)
+	{
+		std::cout << array[index] << ' ';
+	}
+}
+
+int main()
+{
+	int value = 5;
+
+	std::cout << "value = " << value << '\n';
+	foo(&value);
+	std::cout << "value = " << value << '\n';
+
+	int array[6] = { 6, 5, 4, 3, 2, 1 };
+
+	printArray(array, 6);
+	return 0;
+}
