71daad10bbdb751a986258eb54af5a81bd65cf9a
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-07-28 11:09:05 -0500

    Create Main.cpp

---
+++ Concept/FunctionPointer/Main.cpp
@@ -0,0 +1,23 @@
+// fcnPtr is a pointer to a function that takes no arguments and returns an integer
+int(*fcnPtr)();
+
+// const function pointer
+int(*const fcnPtr)();
+
+int foo()
+{
+	return 5;
+}
+
+int goo()
+{
+	return 6;
+}
+
+int main()
+{
+	int(*fcnPtr)() = foo; // fcnPtr points to function foo
+	fcnPtr = goo; // fcnPtr now points to function goo
+
+	return 0;
+}
