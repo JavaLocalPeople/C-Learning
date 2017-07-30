d5998d4af149e0b13376be44c457f614a3a1c991
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-07-30 18:18:33 -0500

    Update exercise1.cpp

--- OOP-PublicPrivate/exercise1.cpp
+++ OOP-PublicPrivate/exercise1.cpp
@@ -15,13 +15,32 @@ public:
 	{
 		std::cout << "<" << m_x << ", " << m_y << ", " << m_z << ">";
 	}
+	
+	bool isEqual(const Point3d &p) {
+		return (m_x == p.m_x && m_y == p.m_y && m_z == p.m_z);
+	}
 };
 
 int main()
 {
-	Point3d point;
-	point.setValues(1.0, 2.0, 3.0);
-	point.print();
+		Point3d point1;
+		point1.setValues(1.0, 2.0, 3.0);
 
-	return 0;
+		Point3d point2;
+		point2.setValues(1.0, 2.0, 3.0);
+
+		if (point1.isEqual(point2))
+			std::cout << "point1 and point2 are equal\n";
+		else
+			std::cout << "point1 and point2 are not equal\n";
+
+		Point3d point3;
+		point3.setValues(3.0, 4.0, 5.0);
+
+		if (point1.isEqual(point3))
+			std::cout << "point1 and point3 are equal\n";
+		else
+			std::cout << "point1 and point3 are not equal\n";
+
+		return 0;
 }
