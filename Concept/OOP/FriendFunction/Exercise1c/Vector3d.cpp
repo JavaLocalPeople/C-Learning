2777d415f6d9d89d0d5d020ca8f625f9643a559b
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-08-01 19:11:08 -0500

    Create Vector3d.cpp

---
+++ Concept/OOP/FriendFunction/Exercise1c/Vector3d.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Vector3d.h" // Vector3d class declared in this file
+
+void Vector3d::print()
+{
+	std::cout << "Vector(" << m_x << " , " << m_y << " , " << m_z << ")\n";
+}
