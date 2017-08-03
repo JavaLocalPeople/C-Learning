937c672dd019399c824a1b90a443e6f2df003e2f
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-08-03 12:36:24 -0500

    Create NegativeOperator.cpp

---
+++ Concept/OverloadingOperator/UnaryOperator/NegativeOperator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Cents
+{
+private:
+	int m_cents;
+
+public:
+	Cents(int cents) { m_cents = cents; }
+
+	// Overload -Cents as a member function
+	Cents operator-() const;
+
+	int getCents() const { return m_cents; }
+};
+
+Cents Cents::operator-() const
+{
+	return Cents(-m_cents);
+}
+
+int main()
+{
+	const Cents nickle(5);
+	std::cout << "A nickle of debt is worth " << (-nickle).getCents() << " cents\n";
+
+	return 0;
+}
