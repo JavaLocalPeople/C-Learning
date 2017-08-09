c183ebb959ba83c07a91388b40169d26d7a36d09
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-08-09 11:21:40 -0500

    Create Main.cpp

---
+++ Concept/VirtualFunction/operatorVirtual/Main.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+#include <string>
+
+class Base
+{
+public:
+	Base() {}
+
+
+	friend std::ostream& operator<<(std::ostream &out, const Base &b)
+	{
+		return b.print(out);
+	}
+
+	virtual std::ostream& print(std::ostream& out) const { 
+		out << "Base";
+		return out;
+	}
+
+};
+
+class Derived : public Base
+{
+public:
+	Derived() : Base() {}
+
+	virtual std::ostream& print(std::ostream& out) const override {
+		out << "Derived"; 
+		return out;
+	}
+
+};
+
+
+int main()
+{
+	Base b;
+	std::cout << b << '\n';
+
+	Derived d;
+	std::cout << d << '\n';
+
+	Base &bref = d;
+	std::cout << bref << '\n';
+
+	return 0;
+}
