9ac04a7b54f0a6d9cb73c734c050b2b755b82fd1
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-08-11 16:32:45 -0500

    Update Main.cpp

--- Cpp for the Impatient/Chpt7-ClassesAndObjects/Exercise/3/Main.cpp
+++ Cpp for the Impatient/Chpt7-ClassesAndObjects/Exercise/3/Main.cpp
@@ -1,2 +1,5 @@
-	ptr[i] &= ~(0x01 << j);
-	ptr[i] |= (0x01 << j);   // Turn it on!
+// cannot use override assignment because: An operator function must either be a member of a class or have at least one parameter of class type.
+
+ptr[i] &= ~(0x01 << j);
+ptr[i] |= (0x01 << j);   // Turn it on!
+
